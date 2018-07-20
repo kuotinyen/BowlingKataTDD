a545b0ad99c32ee9306cd86d23186f2612373cbf
kuotinyen <kuotinyen@gmail.com>
2018-07-20 10:54:56 +0800

    Add game class

---
+++ BowlingKataTDD/Game.swift
@@ -0,0 +1,13 @@
+//
+//  Game.swift
+//  BowlingKataTDD
+//
+//  Created by TING YEN KUO on 2018/7/20.
+//  Copyright © 2018年 TING YEN KUO. All rights reserved.
+//
+
+import Foundation
+
+class Game {
+  
+}

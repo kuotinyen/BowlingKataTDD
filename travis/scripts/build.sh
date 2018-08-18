e90e8bc2b7a0d0e008d8e806ccbed83c5e4324e3
kuotinyen <kuotinyen@gmail.com>
2018-08-18 15:10:03 +0800

    Update goal of travis CI

--- travis/scripts/build.sh
+++ travis/scripts/build.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-xcrun xcodebuild -project BowlingKataTDD.xcodeproj -scheme BowlingKataTDD \
-  -archivePath BowlingKataTDD.xcarchive archive
+# xcrun xcodebuild -project BowlingKataTDD.xcodeproj -scheme BowlingKataTDD \
+  # -archivePath BowlingKataTDD.xcarchive archive
+
+
+
+xcodebuild -project BowlingKataTDD.xcodeproj -scheme BowlingKataTDD -sdk ihponesimulator
+
 
 # xcrun xcodebuild -exportArchive -archivePath BowlingKataTDD.xcarchive \
 #   -exportPath ./build -exportOptionsPlist ExportOptions.plist
